d080051164152789229481b4a363f49fbc3f7eba
IAMABHIJITH123 <jijibnr@gmail.com>
2026-07-18 18:53:08 +0530

    Update server IP address in script.js

--- script.js
+++ script.js
@@ -132,7 +132,7 @@ document.addEventListener('DOMContentLoaded', () => {
     initPixelSnow();
 
     // ====================== SERVER STATUS (UPDATED) ======================
-    const serverIp = 'de25.spaceify.eu:25725';
+    const serverIp = 'play.bramayugam.bond';
     const apiUrl = `https://api.mcstatus.io/v2/status/java/${serverIp}?query=false`;
 
     const statusDot = document.getElementById('status-dot-main');
